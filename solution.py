244b41e1c9ff13ce294df3759be0c912f072572a
Zarina Yeleukina <95769264+zarinayeleukina@users.noreply.github.com>
2023-04-07 21:05:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
+from scipy.stats import chi2
 
 from scipy.stats import norm
 
@@ -11,7 +13,13 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+
+    chi2_rv = chi2(df = 2 * n)
+
+    left = chi2_rv.ppf(1 - alpha / 2)
+    right = chi2_rv.ppf(alpha / 2)
+
+    return np.sqrt(n * x2_mean / (left * 23)), np.sqrt(n * x2_mean / (right * 23))
